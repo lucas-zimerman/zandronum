2b3a5f6b2f406bfbd51331996bc0fdde6e296ad9
Michael Labbe <mike@frogtoss.com>
2016-03-02 02:31:12 -0800

    21:9 aspect ratio support
     - vid_aspect 6 forces 21:9

--- src/g_shared/shared_sbar.cpp
+++ src/g_shared/shared_sbar.cpp
@@ -1079,7 +1079,7 @@ void DBaseStatusBar::RefreshBackground () const
 	int x, x2, y, ratio;
 
 	ratio = CheckRatio (SCREENWIDTH, SCREENHEIGHT);
-	x = (!(ratio & 3) || !Scaled) ? ST_X : SCREENWIDTH*(48-BaseRatioSizes[ratio][3])/(48*2);
+	x = (!IsRatioWidescreen(ratio) || !Scaled) ? ST_X : SCREENWIDTH*(48-BaseRatioSizes[ratio][3])/(48*2);
 	y = x == ST_X && x > 0 ? ST_Y : ::ST_Y;
 
 	if(!CompleteBorder)
@@ -1099,7 +1099,7 @@ void DBaseStatusBar::RefreshBackground () const
 	{
 		if(!CompleteBorder)
 		{
-			x2 = !(ratio & 3) || !Scaled ? ST_X+HorizontalResolution :
+			x2 = !IsRatioWidescreen(ratio) || !Scaled ? ST_X+HorizontalResolution :
 				SCREENWIDTH - (SCREENWIDTH*(48-BaseRatioSizes[ratio][3])+48*2-1)/(48*2);
 		}
 		else
