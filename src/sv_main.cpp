7bff7dbcda5bbf83ad9295b771fdeb0b48263f6c
Adam Kaminski <kaminskiadam9@gmail.com>
2021-11-18 20:39:45 -0500

    Backed out changeset: 6efa764c923f Removes: "Don't perform a backtrace on a player who activated any specials during extrapolation."

--- src/sv_main.cpp
+++ src/sv_main.cpp
@@ -5305,7 +5305,6 @@ CLIENT_PLAYER_DATA_s::CLIENT_PLAYER_DATA_s ( player_t *player )
 	crouchViewDelta = player->crouchviewdelta;
 	bTookEnvironmentalDamage = false;
 	bTeleported = false;
-	bActivatedSpecial = false;
 }
 
 //*****************************************************************************
@@ -7606,11 +7605,6 @@ static bool server_ShouldPerformBacktrace( ULONG ulClient )
 	else if ( g_aClients[ulClient].OldData->bTookEnvironmentalDamage )
 	{
 		reason = "took environmental damage during extrapolation";
-		bShouldPerform = false;
-	}
-	else if ( g_aClients[ulClient].OldData->bActivatedSpecial )
-	{
-		reason = "activated special during extrapolation";
 		bShouldPerform = false;
 	}
 	else if ( gametic - g_aClients[ulClient].OldData->ulSavedGametic >= UNLAGGEDTICS )
