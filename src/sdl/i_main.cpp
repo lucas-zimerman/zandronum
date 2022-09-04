85967439069d36f9e1c8efe553de5b18acc15da8
Benjamin Berkels <torr.samaho@quantentunnel.de>
2022-09-04 22:11:19 +0200

    Fixed: SDL_Init must be called before gtk_init_check (patch from icculus, fixes 4023).

--- src/sdl/i_main.cpp
+++ src/sdl/i_main.cpp
@@ -263,9 +263,10 @@ int main (int argc, char **argv)
 	// Note that the LANG environment variable is overridden by LC_*
 	setenv ("LC_NUMERIC", "C", 1);
 
-#ifndef NO_GTK
-	GtkAvailable = gtk_init_check (&argc, &argv);
-#endif
+// [BB] Patch from icculus for SDL 1.2 compatibility.
+// #ifndef NO_GTK
+//	GtkAvailable = gtk_init_check (&argc, &argv);
+// #endif
 	
 	setlocale (LC_ALL, "C");
 
@@ -301,6 +302,14 @@ int main (int argc, char **argv)
 	}
 
 	atterm (SDL_Quit);
+
+	// [BB] Patch from icculus for SDL 1.2 compatibility.
+	// Make sure gtk_init_check happens _after_ SDL_Init, so SDL will call
+	// XInitThreads() on X11, then GTK+ will talk to Xlib without leaving
+	// mutexes uncreated that the system is going to expect to be initialized.
+#ifndef NO_GTK
+	GtkAvailable = gtk_init_check (&argc, &argv);
+#endif
 
 	{
 		char viddriver[80];
